73afa8c89782992121f2b0914f32bba03b0c7a50
subash-5 <s44995878@gmail.com>
2021-08-09 11:52:19 +0545

    segmentation

---
+++ segmentation/segmentation.py
@@ -0,0 +1,8 @@
+import pixellib
+from pixellib.instance import instance_segmentation
+
+segment_video = instance_segmentation()
+segment_video.load_model("mask_rcnn_coco.h5")
+segment_video.process_video("People.mp4", show_bboxes=True,
+                            frames_per_second=15,
+                            output_video_name="output_video.mp4")
